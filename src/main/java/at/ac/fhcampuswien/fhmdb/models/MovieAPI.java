f4dd8d306af53364160d53abd92a2aef31c792b2
Nour-hash <113339425+Nour-hash@users.noreply.github.com>
2023-04-05 07:10:08 +0200

    trying something's out

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/MovieAPI.java
@@ -1,4 +1,30 @@
 package at.ac.fhcampuswien.fhmdb.models;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import com.google.gson.Gson;
+
+import java.io.IOException;
 
 public class MovieAPI {
+    OkHttpClient client = new OkHttpClient();
+
+    String url = "https://prog2.fh-campuswien.ac.at/movies";
+    String genre = "ACTION";
+    int releaseyear= 1933;
+    double rating = 1;
+
+    Request request = new Request.Builder()
+            .url(url + "?genre=" + genre +"&releaseYear="+releaseyear+ "&ratingFrom=" + rating)
+            .build();
+
+        Response response = client.newCall(request).execute();
+        String jsonData = response.body().string();
+        Movie movieResponse = new Gson().fromJson(jsonData, Movie.class);
+
+
+
+
+    public MovieAPI() throws IOException {
+    }
 }
